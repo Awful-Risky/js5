6340796ceef92020b840a8467babc7abc0478b84
Student <student@RU-102.local>
2016-01-22 15:46:10 -0700

    js5_3

--- script.js
+++ script.js
@@ -3,5 +3,27 @@ angular.module('angMod', [])
 angular.module('angMod').controller('angoller', ['$scope',
 	function($scope) {
 		
+		$scope.textBlock1 = 'This is a text block-------------';
+		$scope.textBlock2 = 'This is a text block-------------';
 		
+
+		$scope.showBlock1 = true;
+		$scope.showBlock2 = true;
+		$scope.showMe = function (id) {
+			if (id == 'block1') {
+				$scope.showBlock1 = !$scope.showBlock1;
+			}
+			else {
+				$scope.showBlock2 = !$scope.showBlock2;
+			}
+		}
+		
+		$scope.hideMe = function (id) {
+			if (id == 'block1') {
+				$scope.showBlock1 = !$scope.showBlock1;
+			}
+			else {
+				$scope.showBlock2 = !$scope.showBlock2;
+			}
+		}
 	}])
